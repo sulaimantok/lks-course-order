ee0c6668e042e2f027cde7ee23a8fe2cc11dd6ed
Sulaiman <sulaimanniceguy@gmail.com>
2023-08-15 16:53:33 +0700

    Update aws.js

--- config/aws.js
+++ config/aws.js
@@ -7,6 +7,7 @@ module.exports = {
       credentials: {
          accessKeyId: process.env.AWS_ACCESS_KEY,
          secretAccessKey: process.env.AWS_SECRET_KEY,
+         sessionToken: process.env.AWS_SESSION_TOKEN,
       },
       region: process.env.AWS_REGION,
    },
